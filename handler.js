ec96c072d7a9fe7a5b2b019bee5735faff1948df
Robert Pelz <pelz.robert@gmail.com>
2018-04-17 23:55:34 +0200

    resizing image functionality implemented

--- handler.js
+++ handler.js
@@ -1,6 +1,8 @@
 'use strict';
 
-module.exports.s3upload = (event, context, callback) => {
+const resizer = require('./resizer');
+
+module.exports.resizer = (event, context, callback) => {
   console.log(event.Records[0].s3);
 
   const bucket = event.Records[0].s3.bucket.name;
@@ -8,6 +10,13 @@ module.exports.s3upload = (event, context, callback) => {
 
   console.log(`A file named ${key} was put in a bucket ${bucket} - NEW!`);
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  callback(null, { message: `A file named ${key} was put in a bucket ${bucket}`, event });
+  resizer(bucket, key)
+    .then(() => {
+      console.log(`The thumbnail was created`);
+      callback(null, { message: `The thumbnail was created` });
+    })
+    .catch(error => {
+      console.log(error);
+      callback(error);
+    });  
 };
